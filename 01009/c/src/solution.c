a7a137261a8549dfe45debaa39be54ea9eb80679
Samuel Andrew Bondoc <sabondoc@uci.edu>
2022-03-10 21:40:54 -0800

    Cleaned directory structure.

--- 01009/c/src/solution.c
+++ 01009/c/src/solution.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char **argv)
+#include <stdint.h>
+int complement_bitwise(int n)
 {
+	int b = 0;	
+	uint32_t m = (uint32_t) n;
+	if (n == 0)
+	{
+		return 1;
+	}
+	while (m < UINT32_C(1) << 31)
+	{
+		b++;
+		m <<= 1;
+	}
+	m = ~m;
+	m >>= b;
+	return m;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", complement_bitwise(5));
 	return EXIT_SUCCESS;
 }
