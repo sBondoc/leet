63b1cc390ce8c31271556edf8d7ac662e99711ff
sBondoc <sabondoc@uci.edu>
2022-03-22 18:32:53 +0000

    Skeleton code.

---
+++ 01663/c/src/solution.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *string_smallest(int n, int k)
+{
+	char *ret = NULL;
+	// code
+	return ret;
+}
+
+int main(int argc, char *argv[])
+{
+	// driver code
+	return EXIT_SUCCESS;
+}
