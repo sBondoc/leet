6cd26856956390ebdc9003fc13bb3652c65352d6
sBondoc <sabondoc@uci.edu>
2022-03-22 18:09:47 +0000

    Added solution for 00763 in C.

--- 00763/c/src/solution.c
+++ 00763/c/src/solution.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define ASCII_OFFSET 97
+
 void print_arr(int *arr, int size)
 {
 	fprintf(stdout, "{");
@@ -13,18 +15,29 @@ void print_arr(int *arr, int size)
 	fprintf(stdout, "%d}\n", arr[size - 1]);
 }
 
+bool in_interval(int n, int a, int b)
+{
+	return (a <= n) && (n <= b);
+}
+
 bool overlap(int start_a, int end_a, int start_b, int end_b)
 {
 	return
-		(0 < start_a) &&
-		(0 < start_b) &&
-		(0 < end_a) &&
-		(0 < end_b) &&
-		((start_a <= end_b) || (start_b <= end_a));
+		(-1 < start_a) &&
+		(-1 < start_b) &&
+		(-1 < end_a) &&
+		(-1 < end_b) &&
+		(
+			in_interval(start_a, start_b, end_b) ||
+			in_interval(end_a, start_b, end_b) ||
+			in_interval(start_b, start_a, end_a) ||
+			in_interval(end_b, start_a, end_a)
+		);
 }
 
 int *labels_partition(char *s, int *size_return)
 {
+	/* Get intervals for each letter. */
 	int interval[26][2], length = strlen(s);
 	for (int i = 0; i < 26; i++)
 	{
@@ -37,7 +50,7 @@ int *labels_partition(char *s, int *size_return)
 		{
 			if (s[i] == c)
 			{
-				interval[c - 97][0] = i;
+				interval[c - ASCII_OFFSET][0] = i;
 				break;
 			}
 		}
@@ -45,20 +58,55 @@ int *labels_partition(char *s, int *size_return)
 		{
 			if (s[i] == c)
 			{
-				interval[c - 97][1] = i;
+				interval[c - ASCII_OFFSET][1] = i;
 				break;
 			}
 		}
 	}
-	return malloc(sizeof(int));
+	int *ret = NULL;
+	*size_return = 0;
+	int start = 0, stop = 0;
+	for (int i = 0; s[i]; i++)
+	{
+		start = i;
+		stop = interval[s[start] - ASCII_OFFSET][1];
+		/* "Absorb" interval if it overlaps with any other letter intervals. */
+		bool expanded = true;
+		while (expanded)
+		{
+			expanded = false;
+			for (char c = 'a'; c < 'z' + 1; c++)
+			{
+				int *interval_c = interval[c - ASCII_OFFSET];
+				if
+				(
+					overlap(start, stop, interval_c[0], interval_c[1]) &&
+					(stop < interval_c[1])
+				)
+				{
+					stop = interval_c[1];
+					/* If expanded, will need to run the check again to see if new interval overlaps any intervals not previously covered. */
+					expanded = true;
+				}
+			}
+		}
+		(*size_return)++;
+		ret = realloc(ret, (*size_return) * sizeof(int));
+		ret[(*size_return) - 1] = stop - start + 1;
+		i = stop;
+	}
+	return ret;
 }
 
 int main(int argc, char *argv[])
 {
-	char *s = "babcbacadefegdehijhklij";
+	char *s = "qiejxqfnqceocmy";
 	int size_return = 0;
 	int *labels = NULL;
+	// fprintf(stdout, "[%d, %d] and [%d, %d] %soverlap\n", 0, 8, 1, 9, overlap(0, 8, 1, 9) ? "" : "do not ");
+	fprintf(stdout, "%s\n", s);
 	labels = labels_partition(s, &size_return);
+	print_arr(labels, size_return);
 	free(labels);
 	return EXIT_SUCCESS;
 }
