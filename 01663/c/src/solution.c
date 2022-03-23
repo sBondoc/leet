969546930238cd70dc6a56fe82a660a5d9408585
sBondoc <sabondoc@uci.edu>
2022-03-23 05:09:48 +0000

    Added solution for 01663 in C.

--- 01663/c/src/solution.c
+++ 01663/c/src/solution.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUFFER 100000
+#define OFFSET_ASCII 97
+
 char *string_smallest(int n, int k)
 {
-	char *ret = NULL;
-	// code
+	static char ret[BUFFER] = {0};
+	int i = n, leftover = k - n;
+	for (ret[i--] = '\0'; 25 < leftover; i--)
+	{
+		ret[i] = 'z';
+		leftover -= 25;
+	}
+	if (0 < leftover)
+	{
+		ret[i] = (char) leftover + OFFSET_ASCII;
+	}
+	for (i--; -1 < i; i--)
+	{
+		ret[i] = 'a';
+	}
 	return ret;
 }
 
 int main(int argc, char *argv[])
 {
-	// driver code
+	fprintf(stdout, "%s\n", string_smallest(2, 18));
 	return EXIT_SUCCESS;
 }
