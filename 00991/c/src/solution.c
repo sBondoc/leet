af1b37e6e987cd44b3b856eef1bdb7fb44971d05
sBondoc <sabondoc@uci.edu>
2022-03-24 05:30:11 +0000

    Skeleton code.

---
+++ 00991/c/src/solution.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int operations_min(int start, int target)
+{
+	int ret = 0;
+	// code
+	return ret;
+}
+	
+int main(int argc, char *argv[])
+{
+	//code
+	return EXIT_SUCCESS;
+}
