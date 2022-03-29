b1b95a16becd6c071d9ceefe6bf32e268c1e1766
sBondoc <sabondoc@uci.edu>
2022-03-29 05:33:22 +0100

    Skeleton code.

---
+++ 00287/c/src/solution.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int duplicate(int *arr, int size)
+{
+	/* code */
+	return -1;
+}
+
+int main(int argc, char *argv[])
+{
+	/* code */
+	return EXIT_SUCCESS;
+}
