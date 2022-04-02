24a62ffafef8834b3346634b5761926bf2d7da4e
sBondoc <sabondoc@uci.edu>
2022-04-02 21:26:16 +0100

    Added brute force solution for 00287 in C.

--- 00287/c/src/solution.c
+++ 00287/c/src/solution.c
@@ -3,12 +3,22 @@
 
 int duplicate(int *arr, int size)
 {
-	/* code */
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if ((i != j) && (arr[i] == arr[j]))
+			{
+				return arr[i];
+			}
+		}
+	}
 	return -1;
 }
 
 int main(int argc, char *argv[])
 {
-	/* code */
+	int arr[] = {3, 1, 3, 4, 2}, size = sizeof(arr) / sizeof(arr[0]);
+	fprintf(stdout, "%d\n", duplicate(arr, size));
 	return EXIT_SUCCESS;
 }
