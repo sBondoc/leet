7ae4fc8793621d9dfe6c1f08fa37cfbd719144c5
sBondoc <sabondoc@uci.edu>
2022-04-03 01:46:21 +0100

    Skeleton code.

---
+++ 00680/c/src/solution.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool palindrome_valid(char *s)
+{
+	// code
+	return false;
+}
+
+int main(int argc, char *argv[])
+{
+	// code
+	return EXIT_SUCCESS;
+}
