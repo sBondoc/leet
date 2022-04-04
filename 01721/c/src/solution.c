2896a8398da50f98c1a237e8df1190e4c4fec061
sBondoc <sabondoc@uci.edu>
2022-04-04 21:12:50 +0100

    Skeleton code.

---
+++ 01721/c/src/solution.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+struct Node
+{
+	int data;
+	struct Node *next;
+};
+
+struct Node *swap(struct Node *head, int k)
+{
+	//code 
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	// code
+	return EXIT_SUCCESS;
+}
